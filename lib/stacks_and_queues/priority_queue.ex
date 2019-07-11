0302d936148fd22681c4c8b00375ff8e58d6a184
James <jamesgood626@gmail.com>
2019-07-11 15:53:05 -0400

    Finished priority queue. Could look into tail recursion. And implement some algs that use a priority queue.

--- lib/stacks_and_queues/priority_queue.ex
+++ lib/stacks_and_queues/priority_queue.ex
@@ -17,9 +17,21 @@ defmodule PriorityQueue do
     %PriorityQueue{length: length, queue: []}
   end
 
-  def add_item(p_queue = %{queue: []}, item), do: p_queue |> Map.put(:queue, [item] )
+  def add_item(p_queue, item) do
+    case check_valid_add(p_queue) do
+      true ->
+        handle_add_item(p_queue, item)
+      false ->
+        {:error, %{message: "The queue is full! You can't add another item."}}
+    end
+  end
 
-  def add_item(p_queue = %{queue: [head | []]}, item = %{priority_level: p_level}) do
+
+  defp check_valid_add(p_queue = %PriorityQueue{length: length, queue: queue}), do: length(queue) < length
+
+  defp handle_add_item(p_queue = %{queue: []}, item), do: p_queue |> Map.put(:queue, [item] )
+
+  defp handle_add_item(p_queue = %{queue: [head | []]}, item = %{priority_level: p_level}) do
     case head.priority_level >= p_level do
       true ->
         p_queue |> Map.put(:queue, [head | [item]])
@@ -28,8 +40,14 @@ defmodule PriorityQueue do
     end
   end
 
-  def add_item(p_queue = %{queue: [head | tail]}, item) do
+  defp handle_add_item(p_queue = %{queue: [head | tail]}, item) do
     p_queue |> Map.put(:queue, priority_level_insert([head], tail, item))
+  end
+
+  def peek(p_queue = %{queue: [head | _tail]}), do: head
+
+  def remove(p_queue = %{queue: [head | tail]}) do
+    {head, p_queue |> Map.put(:queue, tail)}
   end
 
   defp priority_level_insert([new_head | []], [old_head | []], item = %{priority_level: p_level}) do
@@ -44,27 +62,29 @@ defmodule PriorityQueue do
   defp priority_level_insert([new_head | []], [old_head | old_tail], item = %{priority_level: p_level}) do
     case old_head.priority_level < p_level do
       true ->
-        [new_head | [item | [old_head | old_tail]]]
+        List.flatten([new_head | [item | [old_head | old_tail]]])
       false ->
-        priority_level_insert([new_head | [old_head]], old_tail, item)
-    end
-  end
-
-  defp priority_level_insert([new_head | new_tail], [old_head | old_tail], item = %{priority_level: p_level}) do
-    case old_head.priority_level < p_level do
-      true ->
-        [new_head | [new_tail | [item | [old_head | old_tail]]]]
-      false ->
-        priority_level_insert([new_head | [new_tail | [old_head]]], old_tail, item)
+        priority_level_insert(List.flatten([new_head | [old_head]]), old_tail, item)
     end
   end
 
   defp priority_level_insert([new_head | new_tail], [old_head | []], item = %{priority_level: p_level}) do
     case old_head.priority_level < p_level do
       true ->
-        [new_head | [new_tail | [item | [old_head]]]]
+        List.flatten([new_head | [new_tail | [item | [old_head]]]])
       false ->
-        [new_head | [new_tail | [old_head | [item]]]]
+        List.flatten([new_head | [new_tail | [old_head | [item]]]])
     end
   end
+
+  defp priority_level_insert([new_head | new_tail], [old_head | old_tail], item = %{priority_level: p_level}) do
+    case old_head.priority_level < p_level do
+      true ->
+        List.flatten([new_head | [new_tail | [item | [old_head | old_tail]]]])
+      false ->
+        priority_level_insert(List.flatten([new_head | [new_tail | [old_head]]]), old_tail, item)
+    end
+  end
+
+  defp priority_level_insert([new_head | new_tail], [], item), do: List.flatten([new_head | [new_tail | [item]]])
 end
--- test/stacks_and_queues/priority_queue_test.exs
+++ test/stacks_and_queues/priority_queue_test.exs
@@ -2,16 +2,23 @@ defmodule PriorityQueueTest do
   use ExUnit.Case
   doctest PriorityQueue
 
+  @food %{priority_level: 5, item: "Cook Food"}
+  @guitar %{priority_level: 4, item: "Play Guitar"}
+  @workout %{priority_level: 3, item: "Workout"}
+  @clothes %{priority_level: 0, item: "Buy Clothes"}
+  @transit %{priority_level: 0, item: "Take Subway"}
+
   @no_level_queue [
-    %{priority_level: 0, item: "Play Guitar"},
-    %{priority_level: 0, item: "Workout"}
+    @transit,
+    @clothes
   ]
 
   @p_level_queue [
-    %{priority_level: 4, item: "Play Guitar"},
-    %{priority_level: 3, item: "Workout"},
-    %{priority_level: 2, item: "Cook Food"},
-    %{priority_level: 0, item: "Buy Clothes"}
+    @food,
+    @guitar,
+    @workout,
+    @clothes,
+    @transit
   ]
 
   describe "creates a priority queue" do
@@ -31,18 +38,50 @@ defmodule PriorityQueueTest do
   describe "adds to the priority queue" do
     test "new items without a specified priority level" do
       p_queue = PriorityQueue.new()
-      |> PriorityQueue.add_item(%{priority_level: 0, item: "Play Guitar"})
+      |> PriorityQueue.add_item(@transit)
 
-      assert PriorityQueue.add_item(p_queue, %{priority_level: 0, item: "Workout"}) == %PriorityQueue{length: 10, queue: @no_level_queue}
+      assert PriorityQueue.add_item(p_queue, @clothes) == %PriorityQueue{length: 10, queue: @no_level_queue}
     end
 
     test "new items with a specified priority level are added to their proper place" do
       p_queue = PriorityQueue.new()
-      |> PriorityQueue.add_item(%{priority_level: 4, item: "Play Guitar"})
-      |> PriorityQueue.add_item(%{priority_level: 2, item: "Cook Food"})
-      |> PriorityQueue.add_item(%{priority_level: 0, item: "Buy Clothes"})
+      |> PriorityQueue.add_item(@clothes)
+      |> PriorityQueue.add_item(@food)
+      |> PriorityQueue.add_item(@workout)
+      |> PriorityQueue.add_item(@guitar)
 
-      assert PriorityQueue.add_item(p_queue, %{priority_level: 3, item: "Workout"}) == %PriorityQueue{length: 10, queue: @p_level_queue}
+      assert PriorityQueue.add_item(p_queue, @transit) == %PriorityQueue{length: 10, queue: @p_level_queue}
+    end
+
+    test "unless the queue is full, then it will display an error message" do
+      p_queue = PriorityQueue.new(4)
+      |> PriorityQueue.add_item(@clothes)
+      |> PriorityQueue.add_item(@food)
+      |> PriorityQueue.add_item(@workout)
+      |> PriorityQueue.add_item(@guitar)
+
+      assert PriorityQueue.add_item(p_queue, @transit) == {:error, %{message: "The queue is full! You can't add another item."}}
+    end
+  end
+
+  describe "peeks and removes from the queue" do
+    test "peeks successfully" do
+      p_queue = PriorityQueue.new()
+      |> PriorityQueue.add_item(@transit)
+      |> PriorityQueue.add_item(@clothes)
+
+      assert PriorityQueue.peek(p_queue) == @transit
+    end
+
+    test "removes successfully" do
+      p_queue = PriorityQueue.new()
+      |> PriorityQueue.add_item(@transit)
+      |> PriorityQueue.add_item(@clothes)
+
+      {item, p_queue} = PriorityQueue.remove(p_queue)
+
+      assert item == @transit
+      assert p_queue == %PriorityQueue{length: 10, queue: [@clothes]}
     end
   end
 end
